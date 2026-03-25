5f2c94d03053792ba4c023cf22d2efe0eb12c27c
Sophie <227587301+SophieBorgeaud@users.noreply.github.com>
2026-03-25 14:26:20 +0100

    Création de la BD destinations, création des cartes et des premières fonctions d'affichage.

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-/* Carte d'exemple (à reproduire en JS)
-    <article class="film-card">
-        <img src="affiches/inception.jpg" alt="Inception" class="film-poster">
-            <div class="film-info">
-                <h3>Inception</h3>
-                <p class="film-meta">2010 · Science-Fiction</p>
-                <p class="film-realisateur">Christopher Nolan</p>
+"use strict";
+
+// Base de données de destinations
+
+const destinations = [
+    {
+        lieu: ,
+        pays: ,
+        continent: ,
+        type: ,
+        statut: ,
+        date: ,
+        image: ,
+    }
+
+]
+
+// Affiche dans la console une liste de destinations
+
+function afficherDestinationsConsole(liste){
+    console.log("Nombre de destinations :" + liste.length);
+
+    for(const destination of liste){
+
+    }
+}
+
+afficherDestinationsConsole(destinations);
+
+// Recherche de destination dans la liste
+
+function rechercherDestination(liste, terme){
+
+}
+
+
+// Affichage d'une carte de destination
+
+function creerCarteDestination(destination) {
+
+    return `<article class="film-card">
+        <img src="${destination.image}" alt="${destination.lieu}" class="image">
+            <div class="destination-info">
+                <h3>${destination.lieu}</h3>
+                <p class="film-meta">${film.annee} · ${film.genre}</p>
+                <p class="film-realisateur">${film.realisateur}</p>
                 <div class="film-note">
-                    <span class="note-badge">8.8</span>
+                    <span class="note-badge">${film.note}</span>
                 </div>
             </div>
-    </article>
-*/
+    </article>`;
